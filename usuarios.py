9c222af60c68e27042e58e4b810a8f29d238a5e0
Tezk0 <jose.moena.rios@alumnos.sip.cl>
2026-09-21 11:51:15 -0300

    holasixseven

---
+++ usuarios.py
@@ -0,0 +1,39 @@
+from mysqlconnection import connectToMySQL
+DB_NAME = 'usuarios_cert2'
+
+class Usuario:
+    def __init__(self, data):
+        id = data['id']
+        nombre_completo = data['nombre_completo']
+        email = data['email']
+        created_at = data['created_at']
+        updated_at = data['updated_at']
+
+    @classmethod
+    def crear(cls,data):
+        query = "INSERT INTO usuarios (nombre_completo, email) VALUES (%(nombre_completo)s,%(email)s);"
+        return connectToMySQL(DB_NAME).query_db(query, data)
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM usuarios;"
+        resultados = connectToMySQL(DB_NAME).query_db(query)
+
+        usuarios = []
+
+        for usuario in resultados:
+            usuarios.append(cls(usuario))
+
+        return usuarios
+
+    @classmethod
+    def borrar(cls, usuario_id):
+      query = "DELETE FROM usuarios WHERE id = %(id)s"
+      data = {"id": usuario_id}
+
+      resultado = connectToMySQL(DB_NAME).query_db(query, data)
+
+      if resultado is False:
+         raise RuntimeError("Failed to delete user")
+
+      return resultado
